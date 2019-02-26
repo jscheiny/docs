83444b174b79e73462185f8ebccd791b4639ad33
Jonah Scheinerman <jscheinerman@palantir.com>
2019-02-26 14:48:55 -0500

    Move stuff from safe-units

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+interface IDocsConfig {
+    projectName: string;
+    githubUrl: string;
+    inputDir: string;
+    outputDir: string;
+    pageOrder: string[];
+    highlightColorTheme: string;
+}
